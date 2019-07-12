0122f3eff8995136dcea2c20396d34752d9e1bc7
Marcus Comstedt <marcus@mc.pp.se>
2019-07-12 20:44:10 +0200

    Excercise I/O registers in test code

--- rom/source/entry.s
+++ rom/source/entry.s
@@ -1,6 +1,12 @@
 
 	.macpack cbm
 
+
+vscrn = $fa
+vreg  = $fc
+
+
+screen = $400
 	
 	.word start
 	.word start
@@ -30,10 +36,10 @@ init_vic_loop:
 
 clear_screen:
 	lda #$20
-	sta $0400,x
-	sta $0500,x
-	sta $0600,x
-	sta $0700,x
+	sta screen+$000,x
+	sta screen+$100,x
+	sta screen+$200,x
+	sta screen+$300,x
 	lda #$f
 	sta $d800,x
 	sta $d900,x
@@ -45,18 +51,102 @@ clear_screen:
 	ldx #message_length-1
 print_message:	
 	lda message,x
-	sta $0400,x
+	sta screen,x
 	dex
 	bpl print_message
 
 	lda #$1b
 	sta $d011
-		
+
+
+
+	lda #3
+	jsr setrow
+
+	lda #$91
+	sta $dea2
+
+
+	ldx #0
+@dumpdeloop:
+	jsr dumpdereg
+	inx
+	cpx #8
+	bcc @dumpdeloop
+
 
 halt_here:	
 	inc $d020
 	jmp halt_here
 
+
+dumpdereg:
+	lda #>$de00
+	jsr dumpreg
+	ldx vreg
+	jmp nextrow
+
+setrow:
+	ldy #>screen/4
+	sty vscrn+1
+	sta vscrn
+	asl a
+	asl a
+	adc vscrn
+	asl a
+	asl a
+	rol vscrn+1
+	asl a
+	rol vscrn+1
+	sta vscrn
+	ldy #0
+	rts
+
+nextrow:
+	clc
+	lda #40
+	adc vscrn
+	sta vscrn
+	bcc @nocarry
+	inc vscrn+1
+@nocarry:
+	ldy #0
+	rts
+
+dumpreg:
+	sta vreg+1
+	stx vreg
+	jsr printhex
+	lda vreg
+	jsr printhex
+	lda #':'
+	sta (vscrn),y
+	iny
+	lda #' '
+	sta (vscrn),y
+	iny
+	ldx #0
+	lda (vreg,x)
+
+printhex:
+	tax
+	lsr a
+	lsr a
+	lsr a
+	lsr a
+	jsr hexdigit
+	txa
+
+hexdigit:
+	and #$f
+	ora #'0'
+	cmp #10+'0'
+	bcc @numeral
+	sbc #'0'+9
+@numeral:
+	sta (vscrn),y
+	iny
+	rts
 
 vicinit:
 	.byte $0b
